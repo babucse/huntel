356737c06b1835c17d702338d4978fc0a1a102b3
babucse <babucse1991@gmail.com>
2015-07-15 17:16:01 +0530

    Create MovieInfoModel.java

---
+++ src/MovieInfoModel.java
@@ -0,0 +1,19 @@
+package com.loiane.model;
+
+import com.bnym.gsss.common.ui.model.ModelTemplate;
+import com.loiane.bean.MovieInfoBean;
+
+public class MovieInfoModel extends ModelTemplate{
+
+	MovieInfoBean movInfoBean;
+
+	public MovieInfoBean getMovInfoBean() {
+		return movInfoBean;
+	}
+
+	public void setMovInfoBean(MovieInfoBean movInfoBean) {
+		this.movInfoBean = movInfoBean;
+	}
+	
+	
+}
