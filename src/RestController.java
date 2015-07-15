fe3968c4895e36d465753052742321e1d3d8eb87
babucse <babucse1991@gmail.com>
2015-07-15 17:14:57 +0530

    Create RestController.java

---
+++ src/RestController.java
@@ -0,0 +1,56 @@
+package com.loiane.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bnym.gsss.common.annotation.PersistenceConfig;
+import com.bnym.gsss.common.constants.Constants;
+import com.bnym.gsss.common.controller.ControllerTemplateSupport;
+import com.loiane.model.MovieInfoModel;
+
+@Controller
+public class RestController extends ControllerTemplateSupport {
+
+	@RequestMapping(value = "/saveMovieInfo", method = RequestMethod.POST)
+	@PersistenceConfig(enableGenericValidator=false, persistenceHandlerId="movieInfoPersistHndlr", enableEthicalHackValidation = false,formValidatorId = "")
+	public @ResponseBody Object saveUserQuestionnaire(HttpServletRequest request,HttpServletResponse response, @RequestBody MovieInfoModel model) {
+		
+		Map<String, Object> responseInfo = new HashMap<String, Object>();
+		List<String> errorList = new ArrayList<String>();
+		
+		if (super.persistObject(request, response, model, errorList)) {
+				responseInfo.put(Constants.MAP_SUCCESS_KEY, model.getMovInfoBean().getMovieName()+" : Has been saved successfully");	
+		} else {
+			responseInfo.put(Constants.MAP_ERROR_KEY, errorList);
+		}
+		
+		return responseInfo;
+	}
+	
+	@RequestMapping(value = "/getMovieInfo/{movInfoId}", method = RequestMethod.GET)
+	public @ResponseBody Object getMovieInfo(HttpServletRequest request, @PathVariable String movInfoId) {
+		
+		MovieInfoModel model = new MovieInfoModel();
+		boolean isUIFldConfigNeeded = false;
+		String repositoryId = "movieInfoDao";
+		String methodId = "getMovieInfo";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("movInfoId", movInfoId);
+		
+		Map<String, Object> responseMap = super.buildView(request, model, repositoryId, methodId, paramMap, isUIFldConfigNeeded, null);
+
+		return responseMap;
+	}
+}
