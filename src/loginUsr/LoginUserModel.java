80aebb7fcb92a3d37e5a989204c0d27dbdea3a3c
babucse <babucse1991@gmail.com>
2015-07-21 12:51:47 +0530

    Create LoginUserModel.java

---
+++ src/loginUsr/LoginUserModel.java
@@ -0,0 +1,19 @@
+package com.loiane.model;
+
+import com.bnym.gsss.common.ui.model.ModelTemplate;
+import com.loiane.bean.LoginUserBean;
+
+public class LoginUserModel extends ModelTemplate{
+
+	private LoginUserBean loginUsrBean;
+
+	public LoginUserBean getLoginUsrBean() {
+		return loginUsrBean;
+	}
+
+	public void setLoginUsrBean(LoginUserBean loginUsrBean) {
+		this.loginUsrBean = loginUsrBean;
+	}
+	
+	
+}
